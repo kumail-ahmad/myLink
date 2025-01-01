690a7f565a30017d51d78e905fb6e71c33da5d4d
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-01 19:23:57 +0530

    improved UI

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -7,13 +7,19 @@ const Navbar = () => {
       <nav className="bg-white w-[80vw] flex justify-between fixed top-10 right-[10vw] rounded-full p-5 px-7 ">
         <div className="logo&clickables flex items-center gap-3">
           <div className="svg mx-2 flex items-center ">
-            <h3 className="font-bold text-2xl">Palmlink</h3>
-            <Image
-              alt="Palm"
-              width={38}
-              height={32}
-              src="https://img.freepik.com/free-psd/silhouettes-palm-tree-illustration_23-2151074534.jpg?t=st=1735656768~exp=1735660368~hmac=a84f92a2e12d7935b333a73af55bdf7b2b64f906fe48a43de15642c47a30943f&w=740"
-            ></Image>
+            <Link href="/">
+              {" "}
+              <h3 className="font-bold text-2xl">Palmlink</h3>
+            </Link>
+            <Link href="/">
+              {" "}
+              <Image
+                alt="Palm"
+                width={38}
+                height={32}
+                src="https://img.freepik.com/free-psd/silhouettes-palm-tree-illustration_23-2151074534.jpg?t=st=1735656768~exp=1735660368~hmac=a84f92a2e12d7935b333a73af55bdf7b2b64f906fe48a43de15642c47a30943f&w=740"
+              ></Image>
+            </Link>
           </div>
           <div className="clickables ">
             <ul className="clickables  flex flex-row gap-2">
