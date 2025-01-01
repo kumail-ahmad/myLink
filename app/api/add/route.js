c75a5d5bda75d3cd4aa15329f6b5171b431949f5
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-01 15:30:16 +0530

    improved UI

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -1,5 +1,18 @@
+import clientPromise from "@/app/lib/mongodb";
+
 export async function POST(request) {
   const body = await request.json();
+
+  const client = await clientPromise;
+  const db = client.db("Palmlink");
+  const collection = db.collection("links");
+
+  const result = await collection.insertOne(body);
   console.log(body);
-  return Response.json({ message: `Hello World ${body.number}  ` });
+  return Response.json({
+    success: true,
+    error: false,
+    message: "Added",
+    result: result,
+  });
 }
