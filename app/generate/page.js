f073ed82d1c5f492296b4cf6646fd1d4fb1a0edd
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-01 12:40:33 +0530

    added inputs in generate

--- app/generate/page.js
+++ app/generate/page.js
@@ -6,10 +6,10 @@ const Generate = () => {
       <div className="div.picture  ml-44">
         <Image alt="image" src="/generate.png" width={320} height={320} />
       </div>
-      <div className="inputs flex flex-col justify-center items-center">
+      <div className="inputs flex flex-col justify-center items-center gap-2">
         <p>Create your link</p>
-        <input type="text" placeholder="Enter Link text" />
-        <input type="text" placeholder="Enter Link " />
+        <input className="focus:border-purple-400 rounded-full p-3" type="text" placeholder="Enter Link text" />
+        <input className="focus:border-purple-400 rounded-full p-3" type="text" placeholder="Enter Link " />
       </div>
     </div>
   );
