bbf29a4c3472539a065e6be80c6c4e26d4a5fa92
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-01 12:38:19 +0530

    added inputs in generate

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import Image from "next/image";
 const Generate = () => {
   return (
-    <div className="bg-pink-200 min-h-screen grid grid-cols-2">
-      <div className="div.picture">dfbdfdf</div>
+    <div className="bg-[#eac1ea] min-h-screen grid grid-cols-2">
+      <div className="div.picture  ml-44">
+        <Image alt="image" src="/generate.png" width={320} height={320} />
+      </div>
       <div className="inputs flex flex-col justify-center items-center">
         <p>Create your link</p>
-        <input type="text" />
-        <input type="text" />
+        <input type="text" placeholder="Enter Link text" />
+        <input type="text" placeholder="Enter Link " />
       </div>
     </div>
   );
