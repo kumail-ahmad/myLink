aeafd2d9dc4d05b4d5c1390dbd72ef1aac5a00f0
Kumail Ahmad <kumailahmad217@gmail.com>
2025-01-01 12:46:29 +0530

    added inputs in generate

--- app/generate/page.js
+++ app/generate/page.js
@@ -8,9 +8,22 @@ const Generate = () => {
       </div>
       <div className="inputs flex flex-col justify-center items-center gap-2">
         <p className="font-poppins font-semibold text-2xl">Create your link</p>
-        <input className="border border-purple-400 rounded-full p-3" type="text" placeholder="Enter Link text" />
-        <input className="border border-purple-400 rounded-full p-3" type="text" placeholder="Enter Link " />
-        <button className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-3 px-5 rounded-full" type="submit">Submit</button>
+        <input
+          className="border border-purple-400 rounded-full p-3"
+          type="text"
+          placeholder="Enter Link text"
+        />
+        <input
+          className="border border-purple-400 rounded-full p-3"
+          type="text"
+          placeholder="Enter Link Url"
+        />
+        <button
+          className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-3 px-5 rounded-full"
+          type="submit"
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
